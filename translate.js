e685447c1ae560aabee65199f76fa8ec47aa46c1
Rishabh0603 <rishabhtrivedilko.50@gmail.com>
2023-01-29 13:24:32 +0530

    completed

--- translate.js
+++ translate.js
@@ -1,16 +1,28 @@
 
 var btnselector = document.querySelector("#btn-translate")
 var textinput = document.querySelector("#txt-input");
-var outputdiv= document.querySelector("#output");
-
-console.log(btnselector);
-console.log(textinput);
-console.log(outputdiv)
+var outputdiv = document.querySelector("#output");
 
 
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+function getTranslation(text){
+  return serverURL + "?" + "text=" + text
+}
+function errorHandler(error){
+  console.log("error occurred",error)
+}
 
-function eventhandler(){
-    outputdiv.innerText="Banana";
+function clickhandler(){
+ var inputText = textinput.value;
+
+ fetch(getTranslation(inputText))
+  .then(response => response.json())
+  .then(json => {
+    var translatedtext = json.contents.translated;
+    outputdiv.innerText = translatedtext;
+  })
+  .catch(errorHandler)
 };
-btnselector.addEventListener("click",eventhandler);
+btnselector.addEventListener("click",clickhandler);
     
